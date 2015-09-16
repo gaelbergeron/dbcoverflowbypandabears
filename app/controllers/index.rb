f965517d41563b6aa6dd0d8b758522f08b87d38d
aathwal3 <aathwal3@gmail.com>
2015-09-16 15:38:32 -0700

    create login page and new user page includes dummy data for redirect

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,35 @@
+#home page
 get '/' do
   erb :index
 end
 
+#login page
+get '/users/login' do
+	erb :login
+end
+
+#authentication after login
+post '/users/create' do
+	#logic for authentication
+		redirect "/dummy_profile"
+	# redirect "/users/"#{id}"/profile
+end
+
+#new user page
+get '/users/new' do
+	erb :new
+end
+
+#create new user to db 
+post '/users/new' do
+		redirect "/dummy_profile"
+	# redirect "/users/"#{id}"/profile
+end
+
+#logout 
+get '/users/logout' do
+end
+
+get '/dummy_profile' do
+	erb :dummy_profile
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,22 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
 
+  validates :email, :hashed_password, presence: true
+  validates :email, uniqueness: true
+  include BCrypt
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(given_password)
+  	self.password == given_password
+  end
+
+
 end
