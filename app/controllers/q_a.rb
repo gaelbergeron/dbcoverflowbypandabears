492af15a35e596717b4e250bf8ac441407569fa2
Utsab Saha <email": null>
2015-09-16 22:45:18 -0700

    Fixing bug with answers, wasn't adding user id

--- app/controllers/q_a.rb
+++ app/controllers/q_a.rb
@@ -6,21 +6,22 @@ end
 #save new question to db
 post '/questions' do
   params[:user_id] = session[:user_id]
-	question = Question.new(params)
+  question = Question.new(params)
 
-	question.save
-	redirect "/"
+  question.save
+  redirect "/"
 end
 
 # #create a new answer
 # get '/answers/new' do
-# 	erb :'new_answer'
+#   erb :'new_answer'
 # end
 
 #save new answer to db
 post '/questions/:qid/answers' do
   answer = Answer.create({
     "description" => params[:description],
+    "user_id" => session[:user_id],
     "question_id" => params[:qid]
   })
 	redirect "/questions/#{params[:qid]}"
